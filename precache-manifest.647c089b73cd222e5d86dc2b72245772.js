ccdfc0468f2c8e41c75f193ac57fd1d8a120085c
fc0l <fabricolque@hotmail.com>
2020-06-07 12:53:09 -0300

    Updates

---
+++ precache-manifest.647c089b73cd222e5d86dc2b72245772.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee725af9f84136be572271a55887724",
+    "url": "/celebrityRecognition/index.html"
+  },
+  {
+    "revision": "ef28c87f13345380b6d6",
+    "url": "/celebrityRecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4c04444f7c9ca78ddb94",
+    "url": "/celebrityRecognition/static/css/main.b3752e56.chunk.css"
+  },
+  {
+    "revision": "ef28c87f13345380b6d6",
+    "url": "/celebrityRecognition/static/js/2.63baba88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/celebrityRecognition/static/js/2.63baba88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c04444f7c9ca78ddb94",
+    "url": "/celebrityRecognition/static/js/main.13616d03.chunk.js"
+  },
+  {
+    "revision": "6c2c386be4218d9ee6b7",
+    "url": "/celebrityRecognition/static/js/runtime-main.70f059c3.js"
+  }
+]);
